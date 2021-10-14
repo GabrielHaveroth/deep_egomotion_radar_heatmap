e099cc0d721596e0ad199d6c8c5c62f3faf61088
GabrielHaveroth <haverothgabriel@gmail.com>
2021-10-14 00:13:09 -0300

    helpers to interpolate poses, and slice heatmaps

---
+++ helpers.py
@@ -0,0 +1,113 @@
+from scipy.spatial.transform import Rotation, Slerp
+from tensorflow.python.keras.backend import reshape, shape
+from geometric_operations import *
+import scipy
+import scipy.ndimage
+
+MAX_POWER = 4268728.0
+MIN_POWER = 3.081555128097534
+MAX_DOPPLER = 1.1527502536773682
+MIN_DOPPLER = -1.317428708076477
+
+
+def interpolate_poses(src_poses, src_stamps, tgt_stamps):
+    src_start_idx = 0
+    tgt_start_idx = 0
+    src_end_idx = len(src_stamps) - 1
+    tgt_end_idx = len(tgt_stamps) - 1
+
+    # ensure first source timestamp is immediately before first target timestamp
+    while tgt_start_idx < tgt_end_idx and tgt_stamps[tgt_start_idx] < src_stamps[src_start_idx]:
+        tgt_start_idx += 1
+
+    # ensure last source timestamp is immediately after last target timestamp
+    while tgt_end_idx > tgt_start_idx and tgt_stamps[tgt_end_idx] > src_stamps[src_end_idx]:
+        tgt_end_idx -= 1
+
+    # iterate through target timestamps,
+    # interpolating a pose for each as a 4x4 transformation matrix
+    tgt_idx = tgt_start_idx
+    src_idx = src_start_idx
+    tgt_poses = []
+    while tgt_idx <= tgt_end_idx and src_idx <= src_end_idx:
+        # find source timestamps bracketing target timestamp
+        while src_idx + 1 <= src_end_idx and src_stamps[src_idx + 1] < tgt_stamps[tgt_idx]:
+            src_idx += 1
+
+        # get interpolation coefficient
+        c = ((tgt_stamps[tgt_idx] - src_stamps[src_idx]) /
+             (src_stamps[src_idx+1] - src_stamps[src_idx]))
+
+        # interpolate position
+        pose = np.eye(4)
+        pose[:3, 3] = ((1.0 - c) * src_poses[src_idx]
+                       ['position'] + c * src_poses[src_idx+1]['position'])
+
+        # interpolate orientation
+        r_src = Rotation.from_quat([src_poses[src_idx]['orientation'],
+                                    src_poses[src_idx+1]['orientation']])
+        slerp = Slerp([0, 1], r_src)
+        pose[:3, :3] = slerp([c])[0].as_dcm()
+
+        tgt_poses.append(pose)
+
+        # advance target index
+        tgt_idx += 1
+
+    tgt_indices = range(tgt_start_idx, tgt_end_idx + 1)
+    return tgt_poses, tgt_indices
+
+
+def polar2cartesian(outcoords, inputshape, origin):
+    """Coordinate transform for converting a polar array to Cartesian coordinates. 
+    inputshape is a tuple containing the shape of the polar array. origin is a
+    tuple containing the x and y indices of where the origin should be in the
+    output array."""
+
+    xindex, yindex = outcoords
+    x0, y0 = origin
+    x = xindex - x0
+    y = yindex - y0
+
+    r = np.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
+    theta_index = np.round((theta + np.pi) * inputshape[1] / (2 * np.pi))
+
+    return (r, theta_index)
+
+
+def get_range_doppler_slice_heatmap(radar_heatmap):
+    return radar_heatmap[16, :, :, :]
+
+
+def get_cartesian_slice_heatmap(radar_heatmap):
+    radar_heatmap_slice_power = radar_heatmap[16, :, :, 0]
+    radar_heatmap_slice_doppler = radar_heatmap[16, :, :, 1]
+    cartesian_heatmap_power = scipy.ndimage.geometric_transform(radar_heatmap_slice_power, polar2cartesian, order=3,
+                                                                output_shape=(radar_heatmap_slice_power.shape[0],
+                                                                              radar_heatmap_slice_power.shape[1]),
+                                                                extra_keywords={'inputshape': radar_heatmap_slice_power.shape,
+                                                                                'origin': (radar_heatmap_slice_power.shape[0],
+                                                                                           radar_heatmap_slice_power.shape[1] / 2)})
+    cartesian_heatmap_doppler = scipy.ndimage.geometric_transform(radar_heatmap_slice_doppler, polar2cartesian, order=3,
+                                                                  output_shape=(radar_heatmap_slice_doppler.shape[0],
+                                                                                radar_heatmap_slice_doppler.shape[1]),
+                                                                  extra_keywords={'inputshape': radar_heatmap_slice_doppler.shape,
+                                                                                  'origin': (radar_heatmap_slice_doppler.shape[0],
+                                                                                             radar_heatmap_slice_doppler.shape[1] / 2)})
+
+    cartesian_heatmap_power = np.reshape(
+        cartesian_heatmap_power, (128, 128, 1))
+    cartesian_heatmap_doppler = np.reshape(
+        cartesian_heatmap_doppler, (128, 128, 1))
+    cartesian_heatmap = np.concatenate(
+        (cartesian_heatmap_power, cartesian_heatmap_doppler), axis=-1)
+    return cartesian_heatmap
+
+
+def get_range_azimute_slice_heatmap(radar_heatmap):
+    return radar_heatmap[16, :, :, :]
+
+
+def get_range_elevation_slice_heatmap(radar_heatmap):
+    return radar_heatmap[:, 64, :, :]
