c33bdcbb8828ad0d6f1732e3127d395aa083e565
GabrielHaveroth <haverothgabriel@gmail.com>
2021-11-15 09:52:16 -0300

    added data_type input selector

--- data_loader.py
+++ data_loader.py
@@ -1,24 +1,28 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+from tensorflow.python.keras.backend import set_learning_phase
 from dataset_loaders import *
 from helpers import *
 import numpy as np
 from scipy.spatial.transform import Rotation, Slerp
 from geometric_operations import *
 import pandas as pd
+import multiprocessing as mp
 MAX_HEATMAP = np.array([MAX_POWER, MAX_DOPPLER])
 MIN_HEATMAP = np.array([MIN_POWER, MIN_DOPPLER])
 
 
 class RadarEgomotionDataGenerator(keras.utils.Sequence):
-    def __init__(self, df_data, params, batch_size=32, shuffle=True):
+    def __init__(self, df_data, params, batch_size=32, shuffle=True,
+                 data_type="slices"):
         self.batch_size = batch_size
         self.df_data = df_data
         self.shuffle = shuffle
         self.gt_params = get_groundtruth_params()
-        self.heat_map_params = params['heatmap']
+        self.hm_params = params['heatmap']
         self.n = len(df_data)
+        self.data_type = data_type
 
     def __len__(self):
         return int(np.floor(self.n / self.batch_size))
@@ -35,42 +39,72 @@ class RadarEgomotionDataGenerator(keras.utils.Sequence):
             self.df_data = self.df_data.sample(frac=1).reset_index(drop=True)
 
     def __get_gt_pose_by_file(self, names):
-        poses = dict()
-        for name in names:
-            gt_poses = get_groundtruth(name)
-            radar_timestamps = get_timestamps(name, self.heat_map_params)
-            gt_timestamps = get_timestamps(name, self.gt_params)
-            radar_gt, _ = interpolate_poses(gt_poses,
-                                            gt_timestamps,
-                                            radar_timestamps)
-            poses[name] = radar_gt
-        return poses
+        poses_seq = dict()
+        with mp.Pool(mp.cpu_count()) as pool:
+            results = [pool.apply_async(get_heatmap_poses, args=(
+                name, self.hm_params)) for name in names]
+            for r, name in zip(results, names):
+                poses_seq[name] = r.get()
+        return poses_seq
 
-    def __get_data(self, batches):
+    def __get_data(self, batches: pd.DataFrame):
         names = list(batches['file'].unique())
         # Generates data containing batch_size samples
-        poses_per_name = self.__get_gt_pose_by_file(names)
-        cart_heatmapst12 = []
-        elev_radar_heatmapt12 = []
+        poses_seq = self.__get_gt_pose_by_file(names)
         delta_trans = []
         delta_rot = []
-        for _, row in batches.iterrows():
-            seq = row['file']
-            pair = row['pair']
-            poses = poses_per_name[seq]
-            radar_heatmap_t_1 = get_heatmap(pair[0], seq,  self.heat_map_params)
-            radar_heatmap_t_2 = get_heatmap(pair[1], seq,  self.heat_map_params)
-            cart_radar_heatmap_t_1 = (get_cartesian_slice_heatmap(radar_heatmap_t_1) - MIN_HEATMAP) / (MAX_HEATMAP - MIN_HEATMAP)
-            cart_radar_heatmap_t_2 = (get_cartesian_slice_heatmap(radar_heatmap_t_2) - MIN_HEATMAP) / (MAX_HEATMAP - MIN_HEATMAP)
-            elev_radar_heatmap_t_1 = (get_range_elevation_slice_heatmap(radar_heatmap_t_1) - MIN_HEATMAP) / (MAX_HEATMAP - MIN_HEATMAP)
-            elev_radar_heatmap_t_2 = (get_range_elevation_slice_heatmap(radar_heatmap_t_2) - MIN_HEATMAP) / (MAX_HEATMAP - MIN_HEATMAP)
-            cart_heatmapst12.append(np.concatenate([cart_radar_heatmap_t_1, cart_radar_heatmap_t_2], -1))
-            elev_radar_heatmapt12.append(np.concatenate([elev_radar_heatmap_t_1, elev_radar_heatmap_t_2], -1))
-            delta_pose, _ = get_ground_6d_poses_euler(poses[pair[0]], poses[pair[1]])
-            delta_rot.append(delta_pose[0:3])
-            delta_trans.append(delta_pose[3:])
-        X_batch_cart = np.asarray(cart_heatmapst12)
-        X_batch_elev = np.asarray(elev_radar_heatmapt12)
-        y_batch_trans = np.asarray(delta_trans)
-        y_batch_rot = np.asarray(delta_rot)
-        return [X_batch_cart, X_batch_elev], [y_batch_trans, y_batch_rot]
+        if self.data_type == 'custom':
+            cart_heatmapst12 = []
+            elev_radar_heatmapt12 = []
+            for _, row in batches.iterrows():
+                seq = row['file']
+                pair = row['pair']
+                poses = poses_seq[seq]
+                radar_heatmap_t_1 = get_heatmap(
+                    pair[0], seq,  self.heat_map_params)
+                radar_heatmap_t_2 = get_heatmap(
+                    pair[1], seq,  self.heat_map_params)
+                cart_radar_heatmap_t_1 = (get_cartesian_slice_heatmap(
+                    radar_heatmap_t_1) - MIN_HEATMAP) / (MAX_HEATMAP - MIN_HEATMAP)
+                cart_radar_heatmap_t_2 = (get_cartesian_slice_heatmap(
+                    radar_heatmap_t_2) - MIN_HEATMAP) / (MAX_HEATMAP - MIN_HEATMAP)
+                elev_radar_heatmap_t_1 = (get_range_elevation_slice_heatmap(
+                    radar_heatmap_t_1) - MIN_HEATMAP) / (MAX_HEATMAP - MIN_HEATMAP)
+                elev_radar_heatmap_t_2 = (get_range_elevation_slice_heatmap(
+                    radar_heatmap_t_2) - MIN_HEATMAP) / (MAX_HEATMAP - MIN_HEATMAP)
+                cart_heatmapst12.append(np.concatenate(
+                    [cart_radar_heatmap_t_1, cart_radar_heatmap_t_2], -1))
+                elev_radar_heatmapt12.append(np.concatenate(
+                    [elev_radar_heatmap_t_1, elev_radar_heatmap_t_2], -1))
+                delta_pose, _ = get_ground_6d_poses_euler(
+                    poses[pair[0]], poses[pair[1]])
+                delta_rot.append(delta_pose[0:3])
+                delta_trans.append(delta_pose[3:])
+            X_batch_cart = np.asarray(cart_heatmapst12)
+            X_batch_elev = np.asarray(elev_radar_heatmapt12)
+            y_batch_trans = np.asarray(delta_trans)
+            y_batch_rot = np.asarray(delta_rot)
+            data_batch = [[X_batch_cart, X_batch_elev],
+                          [y_batch_trans, y_batch_rot]]
+
+        # Get the original 3D power heatmap elev x azim x range
+        elif self.data_type == '3d_heatmap':
+            delta_poses = []
+            hm_powers_t12 = []
+            with mp.Pool(mp.cpu_count()) as pool:
+                results_async = [pool.apply_async(get_data_3D_heatmap_batch_gt, args=(
+                    row, self.hm_params, poses_seq, )) for _, row in batches.iterrows()]
+                for result in results_async:
+                    hm_power_t12, delta_pose = result.get()
+                    delta_poses.append(delta_pose)
+                    hm_powers_t12.append(hm_power_t12)
+            delta_poses = np.asarray(delta_poses)
+            y_batch_trans = delta_poses[:, 3:].copy()
+            y_batch_rot = delta_poses[:, 0:3].copy()
+            X_batch_power_heatmap = np.array(hm_powers_t12)
+            data_batch = [X_batch_power_heatmap, [y_batch_trans, y_batch_rot]]
+
+        X_batch = data_batch[0]
+        print(type(X_batch))
+        y_batch = data_batch[1]
+        return X_batch, y_batch
